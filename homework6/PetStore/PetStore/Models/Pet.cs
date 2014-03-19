20d8bf37c2640938c3925cff30d6fb31f17a7258
a0mart17 <a0mart17@cardmail.louisville.edu>
2014-03-19 12:04:58 -0700

    pet.cs

--- homework6/PetStore/PetStore/Models/Pet.cs
+++ homework6/PetStore/PetStore/Models/Pet.cs
@@ -1,14 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Data.Entity;
 
 namespace PetStore.Models
 {
     public class Pet
     {
-        public int AnimalID { get; set; }
+        public int ID { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
         public string Category { get; set; }
@@ -17,11 +14,17 @@ namespace PetStore.Models
         public string Gender { get; set; }
         public string Color { get; set; }
         public decimal ListPrice { get; set; }
-        public byte[] Photo { get; set; }
+        public string PhotoUrl { get; set; }
+
+
+
+
     }
 
     public class PetDBContext : DbContext
     {
         public DbSet<Pet> Pets { get; set; }
     }
+
+    
 }
